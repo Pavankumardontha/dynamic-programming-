b4901c90df1bab5906dccafcc6eebc801c5d10b0
Pavankumardontha <pavankumardontha@gmail.com>
2021-06-13 12:16:42 +0530

    Update CoinCombinations-1.cpp

--- CoinCombinations-1.cpp
+++ CoinCombinations-1.cpp
@@ -13,22 +13,21 @@ int main()
     for (int i=0;i<n;i++)
     cin>>coins[i];
     dp[0]=1;
-    /* In coin combination-1, we will the outer loop running from 1 to sum. In coin combination-2 we have the inner 
-    loop running from 1 to sum. Rest everything is same.
+    /* In coin combination-1, we will the outer loop running from 1 to sum. In coin combination-2 we have the inner loop running from 1 to sum. Rest everything is same.
 	If we want to make a sum using coins(each coin is of infinite supply) , we have 2 possibilities 
 	a) Permutations matter(outer loop on sum).
 	b) Only combinations matter(inner loop on sum)
 	We are now solving the case-a
 	*/
     for (int j=1;j<=x;j++)
-	{
+    {
         for (int i=0;i<n;i++)
-		{
+	{
             if (j-coins[i]>=0)
             dp[j]=(dp[j]+dp[j-coins[i]]);
-    }
+        }
         dp[j]%=mod;
- }
+    }
     cout<<dp[x]<<endl;
     return 0;
 }

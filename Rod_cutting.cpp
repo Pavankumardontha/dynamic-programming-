7d7d98efe3565dda4bed23f5551d3f6664d2d0c0
Pavankumardontha <pavankumardontha@gmail.com>
2022-10-25 13:25:58 +0530

    Update Rod_cutting.cpp

--- Rod_cutting.cpp
+++ Rod_cutting.cpp
@@ -30,3 +30,42 @@ class Solution{
         return ans;
     }
 };
+
+/* ---------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+/* DP approach */
+int p[1001];
+int n;
+int dp[1001];
+int solve(int len)
+{
+    if(len==0)
+    return 0;
+    if(dp[len]!=-1)
+    return dp[len];
+    int ans1=0;
+    int ans2=0;
+    /*
+    if we have a rod of length i we can 
+    1) either sell the entire rod of length i or 
+    2) We can try all possible cuts
+    */
+    ans1 = p[len];
+    for(int i=1;i<=len;i++)
+    ans2 = max(ans2,p[i]+solve(len-i));
+    
+    return dp[len] = max(ans1,ans2);
+}
+
+class Solution{
+  public:
+    int cutRod(int price[], int N) 
+    {
+        n=N;
+        memset(dp,-1,sizeof(dp));
+        for(int i=1;i<=n;i++)
+        p[i]=price[i-1];
+        int ans = solve(N);
+        return ans;
+    }
+};

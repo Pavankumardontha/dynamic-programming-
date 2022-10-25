f1e4443bec1498e5a4b793d912af6267c41c63d0
Pavankumardontha <pavankumardontha@gmail.com>
2022-10-25 15:48:48 +0530

    Update MinimumCoinsToMakeChange.cpp

--- MinimumCoinsToMakeChange.cpp
+++ MinimumCoinsToMakeChange.cpp
@@ -1,36 +1,41 @@
-class Solution
+long long int dp[10001];
+int n;
+int coins[10001];
+long long int solve(int value)
 {
+    if(value == 0)
+    return 0;
+    
+    /* At any given point of time we can take any coin if our current_valu - coin value is greater than or equal to 0. */
+    if(dp[value]!=-1)
+    return dp[value];
+    long long int ans = INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        if(value-coins[i] >= 0)
+        {
+            long long int temp = 1 + solve(value-coins[i]);
+            if(temp < ans)
+            ans = temp;
+        }
+    }
+    return dp[value] = ans;
+}
+
+class Solution{
 
 	public:
-	int minCoins(int a[], int n, int sum) 
+	int minCoins(int c[], int M, int V) 
 	{ 
-	   /*You have coins and you have to make a value V with these coins by using 
-	   the minimum no. of coins.*/
-	   int dp[sum+1];
-	   
-	   dp[0]=0;
-	   for(int i=1;i<=sum;i++)
-	   dp[i]=INT_MAX;
-	   
-	   sort(a,a+n);
-	   
-	   
-	   for(int j=1;j<=sum;j++)
-	   {
-	       for(int i=0;i<n;i++)
-	       {
-	           if(j-a[i]>=0)
-	           {
-	               if(dp[j-a[i]]!=INT_MAX)
-	               dp[j]=min(dp[j],1+dp[j-a[i]]);
-	           }
-	       }
-	   }
-	   
-	   if(dp[sum]==INT_MAX)
-	   return -1;
-	   else
-	   return dp[sum];
+	    n = M;
+	    sort(c,c+n);
+	    for(int i=0;i<n;i++)
+	    coins[i]=c[i];
+	    memset(dp,-1,sizeof(dp));
+	    int ans = solve(V);
+	    if(ans == INT_MAX)
+	    return -1;
+	    return ans;
 	} 
 	  
 };

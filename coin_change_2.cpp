77ffa0f236845a1bbefc054a4033b952a7f2ec50
Pavan Kumar <pavankumardontha@gmail.com>
2025-07-08 03:22:12 +0530

    Update coin_change_2.cpp

--- coin_change_2.cpp
+++ coin_change_2.cpp
@@ -39,3 +39,25 @@ public:
         return solve(0,0,coins);
     }
 };
+
+
+// 1D iterative solution
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) 
+    {
+        // only combinations
+        int n = coins.size();
+        vector<unsigned long long int> dp(amount+1,0);
+        dp[0]=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+                if(j-coins[i] >= 0)
+                dp[j] += dp[j-coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+};

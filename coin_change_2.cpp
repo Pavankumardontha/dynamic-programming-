5068f639be8e63b96e12e948fd3a4da9b035b8f1
Pavan Kumar <pavankumardontha@gmail.com>
2025-07-08 14:47:07 +0530

    Update coin_change_2.cpp

--- coin_change_2.cpp
+++ coin_change_2.cpp
@@ -1,3 +1,31 @@
+// iterative DP solution. Note that this is 1D and NOT 2D !!
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) 
+    {
+        // only combinations
+        int n = coins.size();
+        vector<unsigned long long int> dp(amount+1,0);
+        dp[0]=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+                if(j-coins[i] >= 0)
+                dp[j] += dp[j-coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+};
+
+
+// Recursive DP solution - Note that this is 2D DP
+// dp[i][sum] = no. of combinatations possible to make sum by taking elements in the range [i,n]
+/* At any given state, we cannot take any coin from the coins array. We need to take the coins from the right to produce distinct combinations. If order does matter , we can 
+take any coin from the coins array at any given point of time. But here we need to make sure the order does not matter(all the permutations are considered same). So to eliminate
+the permutations, we make sure at any current state (current_ind,current_amount) , we only pick elements in the range [current_ind,n] and not from the range [0,current_ind-1]
+since these produce the same permutations again. Due to this, our recursive DP solution becomes 2D. */
 int n;
 int target;
 int dp[301][5001];
@@ -18,46 +46,3 @@ int solve(int current_ind, int current_sum, vector<int>& coins)
     ans2 = solve(current_ind+1,current_sum,coins);
     return dp[current_ind][current_sum] = ans1+ans2;
 }
-
-
-class Solution {
-public:
-    int change(int amount, vector<int>& coins) 
-    {
-        //dp[i][sum] = no. of combinatations possible to make sum by taking elements in the range [i,n]
-
-        n = coins.size();
-        target = amount;
-        if(amount == 0)
-        return 1;
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=amount;j++)
-            dp[i][j]=-1;
-        }
-        sort(coins.begin(),coins.end());
-        return solve(0,0,coins);
-    }
-};
-
-
-// 1D iterative solution
-class Solution {
-public:
-    int change(int amount, vector<int>& coins) 
-    {
-        // only combinations
-        int n = coins.size();
-        vector<unsigned long long int> dp(amount+1,0);
-        dp[0]=1;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=1;j<=amount;j++)
-            {
-                if(j-coins[i] >= 0)
-                dp[j] += dp[j-coins[i]];
-            }
-        }
-        return dp[amount];
-    }
-};

f38e69b126737c368b4fa8d30c51b10df4e53c2e
Pavan Kumar <pavankumardontha@gmail.com>
2025-07-08 13:56:02 +0530

    Update and rename DiceCombinations(CSES).cpp to dice_combinations.cpp

--- dice_combinations.cpp
+++ dice_combinations.cpp
@@ -2,6 +2,32 @@
 #include<iostream>
 using namespace std;
 
+int main()
+{
+	int n;
+	cin>>n;
+	int dp[n+1]={0};
+	// dp[i] = no. of to construct sum i by throwing a dice one or more times = subproblem = state
+	/* We can reach to the subproblem i by having the previous dice value of 1 or 2....or 6
+	   so dp[i] = dp[i-1]+dp[i-2]+dp[i-3]+dp[i-4]+dp[i-5]+dp[i-6] */
+	int mod = 1e9+7;
+	dp[0]=1;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=6;j++)
+		{
+			if(i-j>=0)
+			dp[i] = (dp[i]%mod + dp[i-j]%mod)%mod;
+		}
+	}
+	cout<<dp[n]<<endl;	
+}
+
+/*-----------------------------------------------------------------*/
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
 // MODEL :- Repetition in taking of elements allowed + Combinations 
 int dp[1000001], mod=1e9+7,sum;
 int solve(int current_sum)

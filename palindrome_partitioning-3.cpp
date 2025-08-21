fa7bdd0e9f227642c171318b4f8a4de731a605b5
Pavan Kumar <pavankumardontha@gmail.com>
2025-08-21 21:15:53 +0530

    Update palindrome_partitioning-3.cpp

--- palindrome_partitioning-3.cpp
+++ palindrome_partitioning-3.cpp
@@ -1,52 +1,60 @@
-int n;
-int k;
-string s;
-int dp[103][103];
-int MinCharacters(string temp)
+/* we will first calculate the no. of changes required for s(i,j) to make it a palindrome using 2d matrix */
+int changes_required[101][101];
+int changesToMakePalindrome(int i, int j, string& s)
 {
-    int n1 = temp.length();
-    int ans = 0;
-    for(int i=0;i<n1/2;i++)
+    // this returns minimum changes to make in substring s(i,j) to make it a palindrome
+    if(i==j or i>j)
+    return changes_required[i][j] = 0;
+    else if(changes_required[i][j]!=-1)
+    return changes_required[i][j];
+    else
     {
-        if(temp[i]!=temp[n1-i-1])
-        ans++;
+        if(s[i]==s[j])
+        return changes_required[i][j] = changesToMakePalindrome(i+1,j-1,s);
+        return changes_required[i][j] = 1 + changesToMakePalindrome(i+1,j-1,s);
     }
-    // if it is palindrome then it returns 0
-    return ans;
 }
-int solve(int index, int splits)
+
+int dp[101][101];
+int solve(int i, int partitions, string& s, int& n, int& k)
 {
-    if(index == n)
+    if(i==n)
     {
-        if(splits == k)
-        return 0;
+        if(partitions==k)
+        return dp[i][partitions] = 0;
         else
-        return 1e8;
+        return dp[i][partitions] = 205; // some random value greater than maximum possible answer value
     }
-    if(splits > k)
-    return 1e8;
-    if(dp[index][splits]!=-1)
-    return dp[index][splits];
-    int left_over_length =  n - index;
-    int ans = 1e8;
-    for(int i=1;i<=left_over_length;i++)
+    else if(dp[i][partitions]!=-1)
+    return dp[i][partitions];
+    else
     {
-        string temp = s.substr(index,i);
-        int min_characters = MinCharacters(temp);
-        ans = min(ans,min_characters + solve(index+i,splits+1));
+        /*
+        by taking the current character , form substrings of different lengths calculate the 
+        no. of changes needed to make it a palindrome using changes_required vector.
+        Add it to the answer and move forward by incrementing the partitions.
+        */
+        int ans = 205; // some random value greater than maximum possible answer value
+        for(int len=1;len<=n-i;len++)
+        ans = min(ans, changes_required[i][i+len-1] + solve(i+len,partitions+1,s,n,k));
+        return dp[i][partitions] = ans;
     }
-    return dp[index][splits]=ans;
 }
 
 class Solution {
 public:
-    int palindromePartition(string s1, int k1) 
+    int palindromePartition(string s, int k) 
     {
-        s = s1;
-        n = s.length();
-        k = k1;
+        int n = s.length();
+        memset(changes_required,-1,sizeof(changes_required));
         memset(dp,-1,sizeof(dp));
-        int ans = solve(0,0);
-        return ans;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                changesToMakePalindrome(i,j,s);
+            }
+        }
+        return solve(0,0,s,n,k);
     }
 };

051f26ce5cd1142e397cc844629d638431bb3bcb
Pavan Kumar <pavankumardontha@gmail.com>
2025-08-21 19:52:07 +0530

    Update palindrome_partitioning-2.cpp

--- palindrome_partitioning-2.cpp
+++ palindrome_partitioning-2.cpp
@@ -1,43 +1,64 @@
-string s;
-int dp[2001],n;
-
-bool IsPalindrome(string s1)
+int is_palindrome[2001][2001]; // to check whether the substring s(i,j) is a palindrome or not
+int dp[2001];
+bool isPalindrome(int i, int j, string& s, int& n)
 {
-    string temp = s1;
-    reverse(temp.begin(),temp.end());
-    return temp == s1;
+    if(is_palindrome[i][j]!=-1)
+    return is_palindrome[i][j];
+    while(i<j)
+    {
+        if(s[i]==s[j])
+        {
+            // since s[i]==s[j] we need to check whether s(i+1,j-1) is a palindrome or not for the substring s(i,j) to be a palindrome
+            return is_palindrome[i][j] = isPalindrome(i+1,j-1,s,n);
+        }
+        return is_palindrome[i][j]=false;
+    }
+    return is_palindrome[i][j]=true;
 }
 
-int solve(int index)
+int solve(int i,string& s, int& n)
 {
-    if(index == n)
-    return 0;
-    int left_over_length = n - index; // left over length including character at position index
-    if(dp[index]!=INT_MAX)
-    return dp[index]; 
-    // if the substring starting at index = index is totally palindrome we need not do any cut
-    if(IsPalindrome(s.substr(index,left_over_length)))
-    return dp[index] = 0;
-    for(int i=1;i<=left_over_length;i++)
+    if(i==n)
+    return dp[i] = 0;
+    else if(dp[i]!=-1)
+    return dp[i];
+    else
     {
-        string temp = s.substr(index,i);
-        if(IsPalindrome(temp))
-        dp[index] = min(dp[index],1 + solve(index+i));
+        // take different lengths substring starting from index i. Check if they are palindromes or not
+        int ans = 3001; // since n<=2000 we are just choosing some random max number >=2000 to compare results
+        for(int len=1;len<=n-i;len++)
+        {
+            // the substring will start at index i and end at index j=i+len-1
+            // check the substring s(i,i+len-1) is a palindrome or not in O(1) time using the is_palindrome matrix. If it a palindrome , put a partition and move forward
+            if(is_palindrome[i][i+len-1])
+            ans = min(ans, 1+solve(i+len,s,n));
+        }
+        return dp[i] = ans;
     }
-    return dp[index];
-
 }
 
 class Solution {
 public:
-    int minCut(string s1) 
+    int minCut(string s) 
     {
-        s = s1;
-        n = s.length();
-        for(int i=0;i<=n;i++)
-        dp[i] = INT_MAX;
-        // memset(dp,INT_MAX,sizeof(dp));
-        int ans = solve(0);
-        return ans;
+        // for each (i,j) subsring in s, find if s(i,j) is a substring or not.
+        // s(i,j) = substring of s starting from index i and ending at index j
+        int n = s.length();
+        int i = 0;
+        memset(dp,-1,sizeof(dp));
+        memset(is_palindrome,-1,sizeof(is_palindrome));
+        /*
+        we will first construct the is_palindrome matrix so answer whether the substring s(i,j)
+        is a palindrome or not */
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                isPalindrome(i,j,s,n);
+            }
+        }
+
+        return solve(0,s,n)-1;
+        
     }
 };
